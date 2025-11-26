5f8a3b8c9ce75bc29a77c311cd20ce42ed791de5
Feitan12 <dolorricky7@gmail.com>
2025-11-26 15:04:33 +0800

    Member 4: updated student age

--- students.sql
+++ students.sql
@@ -4,3 +4,10 @@ CREATE TABLE students (
     grade_level INT
 );
 ALTER TABLE students ADD age INT;
+INSERT INTO students (id, name, grade_level)
+VALUES (1, 'Ana', 10),
+       (2, 'Mark', 11),
+       (3, 'Liza', 12);
+UPDATE students
+SET age = 16
+WHERE id = 1;
